d66a7c95e86928b18c30a0f2cb912385eb225b04
andrade.sampaio <as132476>
2021-02-01 21:24:22 -0300

    Create Project Hello World

---
+++ src/test/java/fit/AppTest.java
@@ -0,0 +1,12 @@
+package fit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldReturnTrue() {
+        Assert.assertTrue(true);
+    }
+}
